1c1a4980fccc4e7e0ae8c21eb9a40b1627e79626
cment <xment92@outlook.com>
2025-02-06 12:59:30 +0200

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js", preload: true
+pin "style", to: "posts/style.js"
