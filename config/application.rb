b3fe0879ce8cb698af820a194b3eb981ff8e9e24
cment <xment92@outlook.com>
2025-02-06 11:23:41 +0200

    Configure and create helpers - Change include_all_helpers config to false - Split the _collapsible_elements.html.erb file's content into   partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Collabfield
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.action_controller.include_all_helpers = false
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
